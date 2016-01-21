59d66af156d3b78415a7154a5317e9bf2a1b2b61
VivianV <voigtviv@hu-berlin.de>
2016-01-21 17:19:49 +0100

    this fixes #48 and some old errors in saltinfo.js

--- src/main/resources/site/js/saltinfo.js
+++ src/main/resources/site/js/saltinfo.js
@@ -16,8 +16,6 @@ function main() {
     /** Creation of boxes around annotation values*/
     $("#content").on("click", ".btn-toggle-box", toggleBox);
 
-    // Load params file (params.json) into global variables
-    loadParams();
     // loads customization file into global variables
     loadCustomization();
 
@@ -32,8 +30,9 @@ var CSV_SEPARATOR = ',';
 var CSV_DOUBLEQUOTE = '"';
 var CSV_LINEBREAK = '\r\n';
 var CSV_MIME_TYPE = 'text/csv';
-var CLASS_OCCURANCE= '.anno-value-count';
+var CLASS_OCCURRENCE= '.anno-value-count';
 var INTERPRET_AS_HTML = false;
+
 /**
  * loads text as data uri
  */
@@ -60,7 +59,7 @@ function convertToCSV(svalues) {
         function() {
             var valuename = escapeDQuote($(this).children(
                 '.svalue-text').text());
-            var valuecount = $(this).children(CLASS_OCCURANCE)
+            var valuecount = $(this).children(CLASS_OCCURRENCE)
                 .text();
             text += CSV_DOUBLEQUOTE + valuename + CSV_DOUBLEQUOTE;
             text += CSV_SEPARATOR;
@@ -80,10 +79,8 @@ function toggleBox(event) {
 }
 
 /*******************************************************************************
- * Load params file (params.json and customization.json) into variables
+ * Load customization file (customization.json) into variables
  ******************************************************************************/
-/** param file contains data of the corpus */
-var FILE_PARAMS = "params.json";
 /** customization file containing user defined vaules to adapt web site */
 var FILE_CUSTOMIZATION = "./customization.json";
 /** Contains the name of the root corpus */
@@ -93,15 +90,17 @@ var shortDescription = "";
 /** Contains a long description of the corpus if given */
 var description = "";
 /** Contains a description of "structuralInfo" if given */
-var structInfoDescription = "";
+var structInfoDesc = "";
 /** Contains a description of "metaDataInfo" if given */
-var metaDataDescription = "";
+var metaDataDesc = "";
 /** Contains a description of "sAnnotationInfo" if given */
-var annoDescription = "";
-/** Contains a description of "sLayerInfo" if given */
-var sLayerDescription = "";
+var annoDesc = "";
+/** A table containing description of layer annotations */
+var layerDesc = [];
 /** Contains an array of author names*/
 var annotators = [];
+/** Contains the license of the corpus**/
+var license = "";
 /** Link to ANNIS instance **/
 var annisLink = null;
 /** A table containing tooltips for metadata **/
@@ -115,6 +114,12 @@ function Author(name, eMail) {
     this.eMail = eMail;
 }
 
+function Layer(name, desc) {
+    this.name = name;
+    this.desc = desc;
+}
+
+
 /** loads customization file and files variables **/
 function loadCustomization() {
     //set the MIME type to json, otherwise firefoy produces a warning
@@ -127,35 +132,52 @@ function loadCustomization() {
     });
     /** load customization file */
     $.getJSON(FILE_CUSTOMIZATION, function(json) {
-        shortDescription = json.shortDescription;
+	corpusName = json.corpusName;        
+	shortDescription = json.shortDescription;
         description = json.description;
+        license = json.license;
+        structInfoDesc = json.structInfoDesc;
+        metaDataDesc = json.metaDataDesc;
+        annoDesc = json.annoDesc;
+        
         if (INTERPRET_AS_HTML){
         	$("#project_tagline").append(shortDescription);
         } else {
-        $("#project_tagline").text(shortDescription);
+            $("#project_tagline").text(shortDescription);
         }
         
-        for (var i = 0; i < json.annotators.length; i++) {
-            annotators[annotators.length] = new Author(json.annotators[i].name, json.annotators[i].eMail);
+       	if (typeof json.annotators!== "undefined"){
+		for (var i = 0; i < json.annotators.length; i++) {
+		    annotators[annotators.length] = new Author(json.annotators[i].name, json.annotators[i].eMail);
+		}
         }
-
+	if (typeof json.layerDesc!== "undefined"){
+		for (var i = 0; i < json.layerDesc.length; i++) {
+			layerDesc[layerDesc.length] = new Layer(json.layerDesc[i].name, json.layerDesc[i].desc);
+		}
+	}
         //load annis links
         annisLink = json.annisLink;
         if (annisLink != null) {
             $("#search_me").css("visibility", "visible");
         }
         // load tooltips for meta data
-		if (json.tooltips_metadata!= null){
-			for (var i = 0; i < json.tooltips_metadata.length; i++) {
-				if (tooltips_metadata== null){
-					tooltips_metadata = new Object();
-				}
+	if (json.tooltips_metadata!= null){
+		for (var i = 0; i < json.tooltips_metadata.length; i++) {
+			if (tooltips_metadata== null){
+				tooltips_metadata = new Object();
+			}
+			if (json.tooltips_metadata[i].tooltip){
 				tooltips_metadata[json.tooltips_metadata[i].name] = json.tooltips_metadata[i].tooltip;
 			}
 		}
-        if (tooltips_metadata== null){
-			console.debug("No tooltips for metadata found in file '"+FILE_CUSTOMIZATION+"'. ");
-		}
+	}
+	
+    if (tooltips_metadata== null){
+		console.debug("No tooltips for metadata found in file '"+FILE_CUSTOMIZATION+"'. ");
+	}
+
+        
 		
 		// load tooltips for annotation names
 		if (json.tooltips_annonames!= null){
@@ -163,14 +185,16 @@ function loadCustomization() {
 				if (tooltips_annonames== null){
 					tooltips_annonames = new Object();
 				}
-				tooltips_annonames[json.tooltips_annonames[i].name] = json.tooltips_annonames[i].tooltip;
+				if (json.tooltips_annonames[i].tooltip){
+					tooltips_annonames[json.tooltips_annonames[i].name] = json.tooltips_annonames[i].tooltip;
+				}
 			}
 		}
         if (tooltips_annonames== null){
 			console.debug("No tooltips for annotation names found in file '"+FILE_CUSTOMIZATION+"'. ");
 		}
-        
-        if (json.tooltips_structuralInfo!= null){
+		
+		 if (json.tooltips_structuralInfo!= null){
 			for (var i = 0; i < json.tooltips_structuralInfo.length; i++) {
 				if (tooltips_structuralInfo== null){
 					tooltips_structuralInfo = new Object();
@@ -185,21 +209,6 @@ function loadCustomization() {
     });
 }
 
-/** loads params file and fills variables */
-function loadParams() {
-        //set the MIME type to json, otherwise firefoy produces a warning
-        $.ajaxSetup({
-            beforeSend: function(xhr) {
-                if (xhr.overrideMimeType) {
-                    xhr.overrideMimeType("application/json");
-                }
-            }
-        });
-        /** load params file */
-        $.getJSON(FILE_PARAMS, function(json) {
-            corpusName = json.corpusName;
-        });
-    }
 /*******************************************************************************
  * Collapse/expand the annotation values corresponding to an annotation name,
  * when there are more annotations as predefined treshhold.
@@ -227,10 +236,8 @@ function loadAndExpandAnnoValues(file, annoName) {
                     }
                 }
             });
-            $.getJSON(file, function(json) {
-                annoTable = json;
-                console.log("-----------------------> loaded: "+file);
-                console.log("-----------------------> loaded: "+JSON.stringify(json));
+	    $.getJSON(file, function(json) {
+		annoTable = json;
                 expandAnnoValues(annoName);
             });
         } else {
@@ -246,23 +253,23 @@ function loadAndExpandAnnoValues(file, annoName) {
  * passed annoName.
  **/
 function expandAnnoValues(annoName) {
-        var $td = $("#"+annoName + "_values");
+        var slot= annoTable[annoName];
+	if (typeof slot=== "undefined"){
+		console.warn("No entry in json found for anno name '"+annoName+"'. ");
+	}
+	var id= "#"+annoName.replace(":", "\\:");
+	
+	var $td = $(id+ "_values");
         var $span = $td.children().eq(0);
-        var slot = annoTable[annoName];
-        
-        console.log("------------> annoTable: "+ JSON.stringify(annoTable));
-        
+
         for (var i = NUM_OF_SET_VALUES; i < slot.length; i++) {
             var $newSpan = $span.clone();
-            
-            console.log("expand add: "+slot[i].value+"("+slot[i].occurance+")");
-            
             $newSpan.children().eq(0).text(slot[i].value);
-            $newSpan.children().eq(1).text(slot[i].occurance);
+            $newSpan.children().eq(1).text(slot[i].occurrence);
             $td.append($newSpan);
         }
 
-        var $btn = $("#" + annoName + "_btn");
+        var $btn = $(id + "_btn");
         $btn.removeClass(SYMBOL_EXPAND);
         $btn.addClass(SYMBOL_COLLAPSE);
         $btn.unbind('click');
@@ -271,26 +278,26 @@ function expandAnnoValues(annoName) {
 /**
  * Collapses the annotation values for the cell corresponding to
  * passed annoName.
+ * For better performance, first all childs of the concerning <td> element 
+ * are removed and second the first NUM_OF_SET_VALUES are added again. This 
+ * is much faster than removing all elements after element NUM_OF_SET_VALUES.
  **/
 function collapseValues(annoName) {
-        var $td = $("#"+annoName + "_values");
-        var numOfChilds= $td.children().length;
-        console.log("collapse numOfChilds: "+ numOfChilds);
-        if (numOfChilds > NUM_OF_SET_VALUES) {
-            //for better performance, first collect all items to be removed and make batch remove
-            var $removalList = $();
-            for (var i = NUM_OF_SET_VALUES; i < numOfChilds; i++) {
-                try {
-					console.log("collapse remove: "+ $td.children().eq(i));
-                    $removalList = $removalList.add($td.children().eq(i));
-                } catch (err) {
-                    console.error(err.message);
-                }
-            }
-            $removalList.remove();
+	var slot= annoTable[annoName];
+	if (typeof slot=== "undefined"){
+		console.warn("No entry in json found for anno name '"+annoName+"'. ");
+	}	
+	var id= "#"+annoName.replace(":", "\\:");
+        var $td = $(id + "_values");
+	var $span = $td.children().eq(0).clone();
+	$td.empty();
+	for (var i = 0; i < NUM_OF_SET_VALUES; i++) {
+	    var $newSpan = $span.clone();
+            $newSpan.children().eq(0).text(slot[i].value);
+            $newSpan.children().eq(1).text(slot[i].occurrence);
+            $td.append($newSpan);
         }
-
-        var $btn = $("#" + annoName + "_btn");
+        var $btn = $(id + "_btn");
         $btn.removeClass(SYMBOL_COLLAPSE);
         $btn.addClass(SYMBOL_EXPAND);
         $btn.unbind('click');
@@ -315,12 +322,17 @@ function styleToolTips() {
         }
     });
 };
+
+
+
+
 /*******************************************************************************
  * Adding tooltips for metadata and annotation names
  ******************************************************************************/
 var CLASS_METADATA="metadata-name";
 var CLASS_ANNO_NAMES="anno-sname";
 var CLASS_TOOLTIP="tooltip";
+var CLASS_LAYER_NAMES="layer-desc";
 /** 
  * Adds tootlips to all elements having the class CLASS_METADATA.
  **/
@@ -362,6 +374,25 @@ function addTooltips_MetaData() {
 		}
 	}
 }
+
+
+function addDescs(){
+        $("#structInfoDescription").append("<p>"+structInfoDesc+"</p>");
+    
+    if (metaDataDesc != null) {
+        $("#metaDataDescription").append("<p>"+metaDataDesc+"</p>");
+    }
+    if (annoDesc != null) {
+        $("#annoDescription").append("<p>"+annoDesc+"</p>");
+    }
+    if (layerDesc != null){
+    	for (var i = 0; i < layerDesc.length; i++) {
+    		document.getElementById(layerDesc[i].name).innerHTML = layerDesc[i].desc;
+    	}
+    }
+}
+
+
 
 /** 
  * Adds tootlips all elements having the class CLASS_METADATA.
@@ -417,6 +448,7 @@ function goANNIS(annoName, annoValue) {
         }
         // create corpus part tu url
         if (corpusName != null) {
+		console.log("--------------> corpusName: "+ corpusName);
             link = link + "_c=" + btoa(corpusName);
         }
         //open link in new window
@@ -441,20 +473,30 @@ function loadMainPage() {
         .load(
             'main.html',
             function() {
-//            	if (corpusName != null) {
-//            		$("#corpusTitle").append(corpusName);
-//            	}
                 if (description != null) {
                 	$("#corpusDescription").append("<p>"+description+"</p>");
                 }
-                if (annotators != null) {
-				    var annotatorElement = $("#annotators");
+                if (license != null) {
+                	$("#license").append("<p>"+license+"</p>");
+                }
+                if (	(annotators != null)&&
+			(annotators.length>0)) {
+			console.log("annotators: "+ annotators);
+		    var annotatorElement = $("#annotators");
                     for (var i = 0; i < annotators.length; i++) {
                         var span = document.createElement('div');
-                        $(span).append(annotators[i].name + ": <a href='mailto:" + annotators[i].eMail + "'>" + annotators[i].eMail + "<a/>");
+                        if (annotators[i].eMail != null) {
+                        	$(span).append(annotators[i].name + ": <a href='mailto:" + annotators[i].eMail + "'>" + annotators[i].eMail + "<a/>");
+                        }
+                        else {
+                        	$(span).append(annotators[i].name + ": no email available");
+                        }
                         $(annotatorElement).append(span);
                     }
-                }
+                }else{
+			console.log("-------------__> EMPTY");
+			$("#annotators").css("visibility", "hidden");
+		}
             });
 }
 
@@ -464,4 +506,12 @@ function loadMainPage() {
 function loadImpressumPage() {
     $('#content')
         .load('impressum.html');
+}
+
+/*******************************************************************************
+ * Load content for help page
+ ******************************************************************************/
+function loadHelpPage() {
+var helpWindow= window.open('help.html', 'popUpWindow', 'height=530,width=530,left=100,top=100,resizable=no, scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no, status=no,titlebar=no,location=no'); 
+helpWindow.focus();
 }
