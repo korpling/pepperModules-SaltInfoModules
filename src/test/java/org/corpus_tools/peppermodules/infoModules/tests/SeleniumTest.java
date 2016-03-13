2f14898e66bb5b56d645efd428478fcd551fc338
florian <f.zipser@gmx.de>
2016-03-13 22:32:31 +0100

    expanded test forselenium

--- src/test/java/org/corpus_tools/peppermodules/infoModules/tests/SeleniumTest.java
+++ src/test/java/org/corpus_tools/peppermodules/infoModules/tests/SeleniumTest.java
@@ -18,6 +18,14 @@ import org.corpus_tools.peppermodules.infoModules.SaltInfoExporter;
 import org.corpus_tools.salt.SaltFactory;
 import org.eclipse.emf.common.util.URI;
 import org.junit.Test;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class SeleniumTest extends PepperExporterTest {
@@ -46,6 +54,77 @@ public class SeleniumTest extends PepperExporterTest {
 		
 		
 	}
+
+	
+	private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	  @Before
+	  public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "/tmp/pepper-test_florian/pepper-test/SaltInfoTest/selenium/index.html";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+
+	  @Test
+	  public void testJUnitWebDriver() throws Exception {
+	    driver.get(baseUrl + "file:///tmp/pepper-test_florian/pepper-test/SaltInfoTest/selenium/index.html");
+	    try {
+	      assertEquals("pcc2", driver.findElement(By.id("project_title")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	    try {
+	      assertEquals("short description of myCorpus", driver.findElement(By.id("project_tagline")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	  }
+
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }
+
 	
 //	@Test
 //	public void test1Doc1Text() throws IOException {
